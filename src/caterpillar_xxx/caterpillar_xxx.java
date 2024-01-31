db15728fc7e1d8acbce6b916bcb9e93a1d10faf9
ArwinMichaelFernandes <arvindfernandes00@gmail.com>
2024-01-31 09:16:23 +0530

    commit 1

---
+++ src/caterpillar_xxx/caterpillar_xxx.java
@@ -0,0 +1,16 @@
+package caterpillar_xxx;
+
+public class caterpillar_xxx {
+    
+	int move,cabbage;
+	
+	caterpillar_xxx(int move,int cabbage){
+		this.move=move;
+		this.cabbage=cabbage;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
